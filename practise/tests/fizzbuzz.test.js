c62895e6f753a6e4d299e4fcbe2387bfb1546883
Alexis Josephides <ajosephides@gmail.com>
2019-08-23 14:10:53 +0100

    feature: multiple of 3 returns Fizz

--- practise/tests/fizzbuzz.test.js
+++ practise/tests/fizzbuzz.test.js
@@ -1,9 +1,17 @@
 const fizzbuzz = require('../fizzbuzz');
 
-test('returns 0', () => {
-  expect(fizzbuzz(0)).toBe(0);
-});
-
 test('returns 1', () => {
   expect(fizzbuzz(1)).toBe(1);
+});
+
+test('returns 2', () => {
+  expect(fizzbuzz(2)).toBe(2);
+});
+
+test('multiple of 3 returns "Fizz"', () => {
+  expect(fizzbuzz(3)).toBe('Fizz');
+});
+
+test('multiple of 3 returns "Fizz"', () => {
+  expect(fizzbuzz(6)).toBe('Fizz');
 });
