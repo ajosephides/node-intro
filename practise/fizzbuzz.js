49f92a2ede52fbb9d410a675e96f429fca8e05ca
Alexis Josephides <ajosephides@gmail.com>
2019-08-23 13:54:01 +0100

    basic fizzbuzz to understand jest tdd

---
+++ practise/fizzbuzz.js
@@ -0,0 +1,7 @@
+function fizzbuzz(number){
+  return number;
+}
+
+
+
+module.exports = fizzbuzz;
---
+++ practise/tests/fizzbuzz.test.js
@@ -0,0 +1,5 @@
+const fizzbuzz = require('../fizzbuzz');
+
+test('returns 0', () => {
+  expect(fizzbuzz(0)).toBe(0);
+});
